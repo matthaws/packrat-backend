b360955a500e5a5150d372f81284ae95886aee16
Matt Haws <matthaws@gmail.com>
2017-12-12 13:01:37 -0500

    Add Category model and migration

--- db/migrate/20171211212850_create_bookmarks.rb
+++ db/migrate/20171211212850_create_bookmarks.rb
@@ -3,8 +3,10 @@ class CreateBookmarks < ActiveRecord::Migration[5.1]
     create_table :bookmarks do |t|
       t.integer :user_id, presence: true
       t.integer :article_id, presence: true
-      
+
       t.timestamps
     end
+
+    add_index :bookmarks, [:user_id, :article_id], unique: true
   end
 end
